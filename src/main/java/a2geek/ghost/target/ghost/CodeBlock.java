0490fedfcefa8e7b9a75c68ac8ac219c4c1b6bba
Rob Greene <robgreene@gmail.com>
2023-04-16 12:10:01 -0500

    Trying to catch accidental nulls earlier in the compilation.

--- src/main/java/a2geek/ghost/target/ghost/CodeBlock.java
+++ src/main/java/a2geek/ghost/target/ghost/CodeBlock.java
@@ -2,6 +2,7 @@ package a2geek.ghost.target.ghost;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class CodeBlock {
     private List<Instruction> codeSegment = new ArrayList<>();
@@ -13,18 +14,26 @@ public class CodeBlock {
         return instructions;
     }
     public void emit(Opcode opcode) {
+        Objects.requireNonNull(opcode, "opcode");
         codeSegment.add(new Instruction(null, opcode, null, null, null));
     }
     public void emit(Opcode opcode, Integer arg) {
+        Objects.requireNonNull(opcode, "opcode");
+        Objects.requireNonNull(arg, "arg");
         codeSegment.add(new Instruction(null, opcode, null, arg, null));
     }
     public void emit(String label) {
+        Objects.requireNonNull(label, "label");
         codeSegment.add(new Instruction(label, null, null, null, null));
     }
     public void emit(Opcode opcode, String label) {
+        Objects.requireNonNull(opcode, "opcode");
+        Objects.requireNonNull(label, "label");
         codeSegment.add(new Instruction(label, opcode, null, null, null));
     }
     public String emitConstant(String suggestedLabel, String string) {
+        Objects.requireNonNull(suggestedLabel, "suggestedLabel");
+        Objects.requireNonNull(string, "string");
         var existing = dataSegment.stream()
                 .filter(inst -> string.equals(inst.string()))
                 .findFirst();
