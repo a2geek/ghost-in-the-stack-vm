576a801de5b1e0f0df685aabe3a0efdcb43d1049
Rob Greene <robgreene@gmail.com>
2023-10-14 10:21:49 -0500

    Adding a pretty print option for the intermediate code since it's useful for development.

--- src/main/java/a2geek/ghost/command/CompileCommand.java
+++ src/main/java/a2geek/ghost/command/CompileCommand.java
@@ -1,9 +1,15 @@
 package a2geek.ghost.command;
 
 import a2geek.ghost.antlr.ParseUtil;
+import a2geek.ghost.model.DataType;
 import a2geek.ghost.model.ModelBuilder;
 import a2geek.ghost.model.Scope;
+import a2geek.ghost.model.StatementBlock;
+import a2geek.ghost.model.scope.Function;
 import a2geek.ghost.model.scope.Program;
+import a2geek.ghost.model.scope.Subroutine;
+import a2geek.ghost.model.statement.IfStatement;
+import a2geek.ghost.model.statement.LabelStatement;
 import a2geek.ghost.model.visitor.ConstantReductionVisitor;
 import a2geek.ghost.model.visitor.DeadCodeEliminationVisitor;
 import a2geek.ghost.model.visitor.StrengthReductionVisitor;
@@ -70,8 +76,11 @@ public class CompileCommand implements Callable<Integer> {
     @ArgGroup(exclusive = false, heading = "Optimizations:%n")
     private OptimizationFlags optimizations = new OptimizationFlags();
 
-    @Option(names = { "-l", "--listing" }, description = "create listing file")
-    private Optional<String> programListing;
+    @Option(names = { "-il", "--intermediate-code-listing" }, description = "create intermediate code listing file")
+    private Optional<String> intermediateCodeListing;
+
+    @Option(names = { "-tl", "--target-code-listing" }, description = "create listing file")
+    private Optional<String> targetCodeListing;
 
     public static String convertControlCharacterMarkers(String value) {
         Pattern pattern = Pattern.compile("<CTRL-(.)>", Pattern.CASE_INSENSITIVE);
@@ -133,6 +142,14 @@ public class CompileCommand implements Callable<Integer> {
             System.out.println("=== REWRITTEN ===");
             System.out.println(program);
         }
+        intermediateCodeListing.ifPresent(filename -> {
+            try {
+                Files.write(Path.of(filename), PrettyPrintVisitor.format(program).getBytes());
+            }
+            catch (IOException ex) {
+                throw new UncheckedIOException(ex);
+            }
+        });
 
         CodeGenerationVisitor codeGenerationVisitor = new CodeGenerationVisitor();
         codeGenerationVisitor.visit(program);
@@ -179,7 +196,7 @@ public class CompileCommand implements Callable<Integer> {
             addr += instruction.size();
         }
 
-        programListing.ifPresent(filename -> {
+        targetCodeListing.ifPresent(filename -> {
             try {
                 Files.write(Path.of(filename), sw.toString().getBytes());
             } catch (IOException e) {
@@ -311,6 +328,104 @@ public class CompileCommand implements Callable<Integer> {
                     loops--;
                 }
             }
+        }
+    }
+
+    public static class PrettyPrintVisitor {
+        public static String format(Program program) {
+            PrettyPrintVisitor visitor = new PrettyPrintVisitor();
+            visitor.formatProgram(program);
+            return visitor.sb.toString();
+        }
+
+        private int indent = 0;
+        private final int indentIncrement = 4;
+        private StringBuilder sb = new StringBuilder();
+
+        public void formatProgram(Program program) {
+            sb.append(String.format("%s:", program.getName()).indent(0));
+            indent += indentIncrement;
+            formatScope(program);
+            formatStatementBlock(program);
+            sb.append("\n");
+            indent -= indentIncrement;
+            program.getScopes().forEach(this::dispatch);
+        }
+
+        public void dispatch(Scope scope) {
+            if (scope instanceof Function func) {
+                formatFunction(func);
+            }
+            else if (scope instanceof Subroutine sub) {
+                formatSubroutine(sub);
+            }
+            else {
+                throw new RuntimeException("unexpected scope type: " + scope.getClass().getName());
+            }
+        }
+
+        public void formatFunction(Function func) {
+            sb.append(String.format("FUNCTION %s(%s) AS %s", func.getName(),
+                    formatParameters(func), func.getDataType()).indent(0));
+            indent += indentIncrement;
+            formatScope(func);
+            formatStatementBlock(func);
+            indent -= indentIncrement;
+            sb.append("END FUNCTION".indent(0));
+            sb.append("\n");
+        }
+
+        public void formatSubroutine(Subroutine sub) {
+            sb.append(String.format("SUB %s(%s)", sub.getName(), formatParameters(sub)).indent(0));
+            indent += indentIncrement;
+            formatScope(sub);
+            formatStatementBlock(sub);
+            indent -= indentIncrement;
+            sb.append("END SUB".indent(0));
+            sb.append("\n");
+        }
+
+        public void formatScope(Scope scope) {
+            scope.getLocalSymbols().stream()
+                    // Only variables
+                    .filter(s -> s.type() == Scope.Type.GLOBAL || s.type() == Scope.Type.LOCAL)
+                    // Strings should have their own DIM statement at this time
+                    .filter(s -> s.dataType() != DataType.STRING)
+                    // Arrays should have their own DIM statement
+                    .filter(s -> s.numDimensions() == 0)
+                    .forEach(s -> {
+                        var fmt = String.format("DIM %s AS %s", s.name(), s.dataType()).indent(indent);
+                        sb.append(fmt);
+                    });
+        }
+
+        public void formatStatementBlock(StatementBlock block) {
+            block.getStatements().forEach(statement -> {
+                if (statement instanceof LabelStatement label) {
+                    sb.append(String.format("%s:", label.getLabel().name()).indent(0));
+                }
+                else if (statement instanceof IfStatement ifStatement) {
+                    sb.append(String.format("IF %s THEN", ifStatement.getExpression()).indent(indent));
+                    indent += indentIncrement;
+                    formatStatementBlock(ifStatement.getTrueStatements());
+                    if (ifStatement.hasFalseStatements()) {
+                        sb.append("ELSE".indent(indent - indentIncrement));
+                        formatStatementBlock(ifStatement.getFalseStatements());
+                    }
+                    indent -= indentIncrement;
+                    sb.append("END IF".indent(indent));
+                }
+                else {
+                    sb.append(statement.toString().indent(indent));
+                }
+            });
+        }
+
+        public String formatParameters(Scope scope) {
+            return scope.getLocalSymbols().stream()
+                    .filter(s -> s.type() == Scope.Type.PARAMETER)
+                    .map(s -> String.format("%s AS %s", s.name(), s.dataType()))
+                    .collect(Collectors.joining(", "));
         }
     }
 }
