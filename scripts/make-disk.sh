b2b14d75d699150ea0f16ec3bf14f7b075f671f1
Rob Greene <robgreene@gmail.com>
2023-11-12 16:25:37 -0600

    Updating disk build script to allow options files on a per-program basis.

--- scripts/make-disk.sh
+++ scripts/make-disk.sh
@@ -3,8 +3,8 @@
 set -eu
 
 GHOST="java -jar build/libs/GhostBasic-1.0-SNAPSHOT.jar"
-ACX_JAR="AppleCommander-acx-1.8.0.jar"
-ACX_URL="https://github.com/AppleCommander/AppleCommander/releases/download/1.8.0/${ACX_JAR}"
+ACX_JAR="AppleCommander-acx-1.9.0.jar"
+ACX_URL="https://github.com/AppleCommander/AppleCommander/releases/download/1.9.0/${ACX_JAR}"
 if [ ! -f build/libs/${ACX_JAR} ]
 then
   echo "Pulling copy of 'acx'..."
@@ -23,16 +23,19 @@ do
   do
     echo "Building file ${source} in ${dir}..."
 
-    filename=$(basename ${source})
+    filename=$(basename "${source}")
     extension="${filename##*.}"
     filename="${filename%.*}"
     filename="${filename//-/}"
+    asfilename="${filename}.as"
+    optname="${source/\.${extension}/\.opt}"
 
     FLAGS="--fix-control-chars"
-    [ "${extension}" == "int" ] && FLAGS="${FLAGS} --integer"
+    [ "${extension}" == "int" ] && FLAGS+=" --integer"
+    [ -f "${optname}" ] && FLAGS+=" @${optname}"
 
-    ${GHOST} $source ${FLAGS} "$@" --output=${filename} --quiet
-    ${ACX} import --dir=${dir} ${filename} --as -a 0x803 -f
-    rm ${filename}
+    ${GHOST} "$source" ${FLAGS} "$@" --output="${asfilename}" --quiet
+    ${ACX} import --dir="${dir}" --name="${filename}" "${asfilename}" --as -a 0x803 -f
+    rm "${asfilename}"
   done
 done
