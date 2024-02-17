7e4369582df2592978ec50abd238989f99fc0047
Rob Greene <robgreene@gmail.com>
2024-02-17 10:09:43 -0600

    Now replacing and removing unneeded symbols.

--- src/main/java/a2geek/ghost/model/visitor/TempVariableConsolidationVisitor.java
+++ src/main/java/a2geek/ghost/model/visitor/TempVariableConsolidationVisitor.java
@@ -5,9 +5,7 @@ import a2geek.ghost.model.expression.*;
 import a2geek.ghost.model.scope.Program;
 import a2geek.ghost.model.visitor.StatementVisitors.ExpressionTracker;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 import static a2geek.ghost.model.Symbol.in;
 import static a2geek.ghost.model.visitor.StatementVisitors.dispatchToExpression;
@@ -21,12 +19,14 @@ public class TempVariableConsolidationVisitor implements ProgramVisitor {
 
     public void dispatch(Scope scope) {
         var tracker = new SymbolRangeTracker();
+        // Identify ranges of all variables
         dispatchToExpression(scope.getInitializationStatements(), tracker, this::captureActiveRanges);
         dispatchToExpression(scope.getStatements(), tracker, this::captureActiveRanges);
-        System.out.printf("SCOPE = %s\n", scope.getFullPathName());
-        tracker.ranges.forEach((symbol,range) -> {
-            System.out.printf("%-15.15s ==> %d..%d\n", symbol.name(), range.min(), range.max());
-        });
+        // Reassign temporary variables
+        dispatchToExpression(scope.getInitializationStatements(), tracker, this::reassignTempVariables);
+        dispatchToExpression(scope.getStatements(), tracker, this::reassignTempVariables);
+        // Whatever temp variables are remaining/unused, we can delete from scope
+        tracker.unused().forEach(scope.getLocalSymbols()::remove);
     }
 
     public Optional<Expression> captureActiveRanges(Expression expression, VisitorContext ctx, SymbolRangeTracker tracker) {
@@ -49,17 +49,57 @@ public class TempVariableConsolidationVisitor implements ProgramVisitor {
         return Optional.empty();
     }
 
+    public Optional<Expression> reassignTempVariables(Expression expression, VisitorContext ctx, SymbolRangeTracker tracker) {
+        switch (expression) {
+            case AddressOfFunction addrOf -> {
+                expression = new AddressOfFunction(tracker.replacement(addrOf.getSymbol(), ctx));
+            }
+            case ArrayLengthFunction arrayLen -> {
+                expression = new ArrayLengthFunction(
+                    tracker.replacement(arrayLen.getSymbol(), ctx),
+                    arrayLen.getDimensionNumber());
+            }
+            case BinaryExpression bin -> {
+                reassignTempVariables(bin.getL(), ctx, tracker.create(ctx)).ifPresent(bin::setL);
+                reassignTempVariables(bin.getR(), ctx, tracker.create(ctx)).ifPresent(bin::setR);
+            }
+            case BooleanConstant ignored -> {}
+            case FunctionExpression func -> {
+                for (int i=0; i<func.getParameters().size(); i++) {
+                    var param = reassignTempVariables(func.getParameters().get(i), ctx, tracker);
+                    if (param.isPresent()) {
+                        func.getParameters().set(i, param.get());
+                    }
+                }
+            }
+            case IntegerConstant ignored -> {}
+            case PlaceholderExpression ignored -> {}
+            case StringConstant ignored -> {}
+            case UnaryExpression unary -> {
+                reassignTempVariables(unary.getExpr(), ctx, tracker).ifPresent(unary::setExpr);
+            }
+            case VariableReference ref -> {
+                expression = VariableReference.with(tracker.replacement(ref.getSymbol(), ctx));
+            }
+            default -> throw new RuntimeException("[compiler bug] unsupported expression type: " + expression);
+        }
+        return Optional.of(expression);
+    }
+
     public static class SymbolRangeTracker implements ExpressionTracker<SymbolRangeTracker> {
         private SymbolRangeTracker parent;
         private VisitorContext parentContext;
-        private Map<Symbol,Range> ranges = new HashMap<>();
+        private final Map<Symbol,Range> ranges = new HashMap<>();
+        private final Map<Symbol,Symbol> replacements = new HashMap<>();
+        private final Set<Symbol> available = new HashSet<>();
 
         @Override
         public SymbolRangeTracker create(VisitorContext ctx) {
-            var tracker = new SymbolRangeTracker();
-            tracker.parent = this;
-            tracker.parentContext = ctx;
-            return tracker;
+            // Tracking into sub blocks is problematic; for now just tracking at the scope level
+            // FIXME left all the parts to support sub blocks; need to resolve at some point
+            // Primary issue tracking is the line number conundrum. It starts fresh within an IF
+            // statement since it is simply the index into the List<Statement>.
+            return this;
         }
 
         public void merge(Symbol symbol, VisitorContext ctx) {
@@ -72,6 +112,41 @@ public class TempVariableConsolidationVisitor implements ProgramVisitor {
                     ranges.merge(symbol, Range.of(ctx.getIndex()), Range::merge);
                 }
             }
+        }
+
+        public Set<Symbol> unused() {
+            var unused = new HashSet<>(ranges.keySet());
+            unused.removeAll(replacements.values());
+            unused.removeAll(available);
+            return unused;
+        }
+
+        public Symbol replacement(Symbol symbol, VisitorContext ctx) {
+            if (symbol.temporary() && symbol.dataType() == DataType.INTEGER) {
+                // Clean up symbols that are out of scope; need to use iterator to modify source Map
+                var iterator = replacements.entrySet().iterator();
+                while (iterator.hasNext()) {
+                    var entry = iterator.next();
+                    if (ctx.getIndex() > ranges.get(entry.getKey()).max()) {
+                        available.add(entry.getValue());
+                        iterator.remove();
+                    }
+                }
+                // Have we seen the symbol already?
+                if (replacements.containsKey(symbol)) {
+                    return replacements.get(symbol);
+                }
+                // No. Do we have any unused variables?
+                if (!available.isEmpty()) {
+                    var replacement = available.stream().findAny().orElseThrow();
+                    available.remove(replacement);
+                    replacements.put(symbol, replacement);
+                    return replacement;
+                }
+                // We should be able to just pick this symbol as one to preserve
+                replacements.put(symbol, symbol);
+            }
+            return symbol;
         }
     }
 
